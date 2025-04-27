b5c11f0904f43114863dcd711c229d5a6708630b
NickelNox <55615334+Marvel202@users.noreply.github.com>
2025-04-27 07:23:56 +0000

    endpoint created

--- ecommerce/app/api/users/[id]/cart/route.ts
+++ ecommerce/app/api/users/[id]/cart/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest } from 'next/server';
 import { products } from '@/app/product-data';
 
-type ShoppingCart = Record<string, string[]>
+type ShoppingCart = Record<string, string[]>;
 
 const carts: ShoppingCart = {
-  '1': ['123','234'],
-  '2': ['345','123'],
-  '3': ['234']
+  '1': ['123', '234'],
+  '2': ['345', '456'],
+  '3': ['234'],
 }
 
 type Params = {
@@ -17,8 +17,13 @@ export async function GET(request: NextRequest, { params }: { params: Params }) 
   const userId = params.id;
   const productIds = carts[userId];
 
-  if (products === undefined) {
-    
+  if (productIds === undefined) {
+    return new Response(JSON.stringify([]), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   }
 
   const cartProducts = productIds.map(id => products.find(p => p.id === id));
@@ -26,7 +31,43 @@ export async function GET(request: NextRequest, { params }: { params: Params }) 
   return new Response(JSON.stringify(cartProducts), {
     status: 200,
     headers: {
-      'Content-Type':'application/json'
+      'Content-Type': 'application/json'
     }
   });
+}
+
+type CartBody = { 
+  productId: string 
+}
+
+export async function POST(request: NextRequest, { params }: { params: Params }) {
+  const userId = params.id;
+  const body: CartBody = await request.json();
+  const productId = body.productId;
+
+  carts[userId] = carts[userId] ? carts[userId].concat(productId) : [productId];
+  const cartProducts = carts[userId].map(id => products.find(p => p.id === id));
+
+  return new Response(JSON.stringify(cartProducts), {
+    status: 201,
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+}
+
+export async function DELETE(request: NextRequest, { params }: { params: Params }) {
+  const userId = params.id;
+  const body = await request.json();
+  const productId = body.productId;
+
+  carts[userId] = carts[userId] ? carts[userId].filter(pid => pid !== productId) : [];
+  const cartProducts = carts[userId].map(id => products.find(p => p.id === id));
+
+  return new Response(JSON.stringify(cartProducts), {
+    status: 202,
+    headers: {
+     'Content-Type': 'application/json',
+    }
+  })
 }
