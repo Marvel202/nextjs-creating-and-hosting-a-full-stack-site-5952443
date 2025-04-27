af263ad0ef62b02f1566314e4cf988dd4176043f
NickelNox <55615334+Marvel202@users.noreply.github.com>
2025-04-27 15:15:59 +0000

    ingestion

---
+++ ecommerce/app/api/db.ts
@@ -0,0 +1,25 @@
+
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+export async function connectToDb() {
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.yll1bfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+
+  try {
+    await client.connect();
+  } finally {
+    await client.close();
+  }
+}
+
+
+
+
