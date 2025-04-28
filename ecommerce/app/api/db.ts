74a38c96a72a4505b8a78459f495d67ca7f08727
NickelNox <55615334+Marvel202@users.noreply.github.com>
2025-04-28 15:25:22 +0000

    add mongodb

--- ecommerce/app/api/db.ts
+++ ecommerce/app/api/db.ts
@@ -1,9 +1,19 @@
+import { Db, MongoClient, ServerApiVersion } from 'mongodb';
 
-import { MongoClient, ServerApiVersion } from 'mongodb';
+let cachedClient: MongoClient | null = null;
+let cachedDb : Db | null = null;
 
 export async function connectToDb() {
-  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.yll1bfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb};
+  }  
+  /*
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.yll1bfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+*/
+
+  const uri ='mongodb+srv://rwtkoon:mnIRcxXQgnc40nSR@cluster0.yll1bfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -11,15 +21,11 @@ export async function connectToDb() {
       deprecationErrors: true,
     }
   });
-
-
-  try {
+  
     await client.connect();
-  } finally {
-    await client.close();
-  }
+
+    cachedClient = client;
+    cachedDb = client.db('Nextjs-ecommerce-site');
+
+    return { client, db: client.db('Nextjs-ecommerce-site')}
 }
-
-
-
-
