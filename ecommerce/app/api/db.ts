408b9d7473240b807b28e09692f15fbd50946793
NickelNox <55615334+Marvel202@users.noreply.github.com>
2025-05-01 07:28:47 +0000

    removed hello folder

--- ecommerce/app/api/db.ts
+++ ecommerce/app/api/db.ts
@@ -8,13 +8,10 @@ export async function connectToDb() {
   if (cachedClient && cachedDb) {
     return { client: cachedClient, db: cachedDb};
   }  
-  /*
-  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.yll1bfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-*/
-
-  const uri ='mongodb+srv://rwtkoon:mnIRcxXQgnc40nSR@cluster0.yll1bfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
   
-  const client = new MongoClient(uri, {
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.yll1bfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  
+const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
--- ecommerce/app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-// app/api/hello/route.ts
-import { getHelloMessage } from '@/lib/hello';
-
-export async function GET() {
-  const data = await getHelloMessage();
-  return Response.json(data);
-}
